5234c24b15c115373549f27adc8bf1d2fa82a4c1
lynn ftmk <alynn412@gmail.com>
2018-08-15 16:34:29 +0800

    first commit

---
+++ app/src/main/java/my/edu/utem/simplerecyclerview/MainActivity.java
@@ -0,0 +1,62 @@
+package my.edu.utem.simplerecyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        CustomAdapter adapter = new CustomAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    //(4)buat find by id
+    public class CustomViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView wordTextView;
+
+        public CustomViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.custom_row,parent,false));
+            wordTextView = itemView.findViewById(R.id.wordTextView);
+        }
+    }
+
+    //(5)what, where, how data display
+    public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder>
+    {
+        public CustomAdapter()
+        {}
+
+        @NonNull
+        @Override
+        //which viewholder to use
+        public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new CustomViewHolder(LayoutInflater.from(viewGroup.getContext()),viewGroup);
+        }
+
+        @Override
+        //what to show for each row
+        public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, int i) {
+            customViewHolder.wordTextView.setText("This is row "+i);
+        }
+
+        @Override
+        //how many rows to repeat
+        public int getItemCount() {
+            return 10;
+        }
+    }
+}
